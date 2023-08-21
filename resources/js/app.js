5f1947c8fa99ecb4ce6c571a703ac384541fd813
ing-gustavo <ing.gustavo.rios.87@gmail.com>
2023-08-20 20:55:41 -0600

    fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,9 +7,9 @@ window.Alpine = Alpine;
 Alpine.start();
 
 import {createApp} from 'vue/dist/vue.esm-bundler.js';
-import PhotosCreate from '../components/photos/Create.vue';
-import PhotosEdit from '../components/photos/Edit.vue';
-import PhotosConfirm from '../components/photos/Confirmar.vue';
+import PhotosCreate from '../components/Photos/Create.vue';
+import PhotosEdit from '../components/Photos/Edit.vue';
+import PhotosConfirm from '../components/Photos/Confirmar.vue';
 
 const app = createApp({});
 app.component('PhotosCreate', PhotosCreate);
